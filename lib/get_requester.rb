6c6781afbf900368f5b2dcb2584f2d829f790872
Nick Nobile <nickcnobile@gmail.com>
2020-10-18 18:08:33 +0000

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,25 @@
-# Write your code here
+require 'pry'
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+
+def initialize(url)
+  @URL = URL
+end
+
+def get_response_body
+  uri = URI.parse(URL)
+  response = Net::HTTP.get_response(uri)
+  response.body
+end
+
+def parse_json
+  people = JSON.parse(self.get_response_body)
+  people.each do |person|
+    person["people"]
+  end
+end
+
+end
