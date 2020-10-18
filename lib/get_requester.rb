c83038a5f018d2bb6c9457fa96ffb121038eca65
Nick Nobile <nickcnobile@gmail.com>
2020-10-18 17:58:05 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -5,7 +5,7 @@ require 'json'
 class GetRequester
 
 def initialize
-  @URL = URL
+  @URL = URL("https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json")
 end
 
 def get_response_body
